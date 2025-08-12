f337840e8aac34aaf972a9033e29a0173eae7d68
Huy Phatt <phatle219@gmail.com>
2025-08-12 21:58:33 +0700

    E25

--- Assets/_Data/Enemy/_Scripts/EnemyController.cs
+++ Assets/_Data/Enemy/_Scripts/EnemyController.cs
@@ -15,14 +15,17 @@ public class EnemyController : SaiMonoBehaviour
     protected virtual void LoadNavMeshAgent()
     {
         if (this.agent != null) return;
-        this.agent = GetComponent<NavMeshAgent>();  
+        this.agent = GetComponent<NavMeshAgent>();
+        this.agent.speed = 2f;
+        this.agent.angularSpeed = 200f;
+        this.agent.acceleration = 150f;
         Debug.Log(transform.name + ":LoadNavMeshAgent", gameObject);
     }
     protected virtual void LoadModel()
     {
         if (this.model != null) return;
         this.model = transform.Find("Model");
-        this.model.localPosition = new Vector3(0, -0.6f, 0);
+        this.model.localPosition = new Vector3(0, 0, 0);
         Debug.Log(transform.name + ":LoadModel", gameObject);
     }
 }
