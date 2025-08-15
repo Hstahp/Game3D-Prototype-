cc4675f5b6b445c60c6a8ad708132b2427cfc311
Huy Phatt <phatle219@gmail.com>
2025-08-15 13:25:11 +0700

    E30 & E31

---
+++ Assets/_Data/Tower/_Scripts/TowerController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class TowerController : SaiMonoBehaviour
+{
+    [SerializeField] protected Transform model;
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadModel();
+    }
+    protected virtual void LoadModel()
+    {
+        if (this.model != null) return;
+        this.model = transform.Find("Model");
+        Debug.Log(transform.name + ": LoadModel", gameObject);
+    }
+}
