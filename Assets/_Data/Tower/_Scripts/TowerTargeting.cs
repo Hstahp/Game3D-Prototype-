767ed6896e1a4ebab59c8f266db84cacd5e50b9d
Huy Phatt <phatle219@gmail.com>
2025-08-21 21:26:01 +0700

    E52

--- Assets/_Data/Tower/_Scripts/TowerTargeting.cs
+++ Assets/_Data/Tower/_Scripts/TowerTargeting.cs
@@ -1,89 +1,124 @@
-using NUnit;
 using System.Collections.Generic;
 using UnityEngine;
+
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(SphereCollider))]
 public class TowerTargeting : SaiMonoBehaviour
 {
     [SerializeField] protected SphereCollider sphereCollider;
     [SerializeField] protected Rigidbody rigid;
+
     [SerializeField] protected EnemyController nearest;
     public EnemyController Nearest => nearest;
+    [SerializeField] protected LayerMask obstacleLayerMask;
+
     [SerializeField] protected List<EnemyController> enemies = new();
+
     protected virtual void FixedUpdate()
     {
         this.FindNearest();
         this.RemoveDeadEnemy();
     }
+
     protected virtual void OnTriggerEnter(Collider collider)
     {
         this.AddEnemy(collider);
     }
+
     protected virtual void OnTriggerExit(Collider collider)
     {
         this.RemoveEnemy(collider);
     }
+
     protected override void LoadComponents()
     {
         base.LoadComponents();
         this.LoadSphereCollider();
         this.LoadRigidbody();
     }
+
     protected virtual void LoadSphereCollider()
     {
         if (this.sphereCollider != null) return;
         this.sphereCollider = GetComponent<SphereCollider>();
-        this.sphereCollider.radius = 5f;
+        this.sphereCollider.radius = 15f;
         this.sphereCollider.isTrigger = true;
         Debug.Log(transform.name + ": LoadSphereCollider", gameObject);
     }
+
     protected virtual void LoadRigidbody()
     {
         if (this.rigid != null) return;
         this.rigid = GetComponent<Rigidbody>();
         this.rigid.useGravity = false;
-         Debug.Log(transform.name + ": LoadSphereCollider", gameObject);
+        Debug.Log(transform.name + ": LoadRigidbody", gameObject);
     }
-    
+
     protected virtual void AddEnemy(Collider collider)
     {
         if (collider.name != Const.TOWER_TARGETABLE) return;
         EnemyController enemyCtrl = collider.transform.parent.GetComponent<EnemyController>();
-        
+
         if (enemyCtrl.EnemyDamageReceiver.IsDead()) return;
+
         this.enemies.Add(enemyCtrl);
     }
+
+
     protected virtual void RemoveEnemy(Collider collider)
     {
-        foreach(EnemyController enemyController in this.enemies)
+        foreach (EnemyController enemyCtrl in this.enemies)
         {
-            if(collider.transform.parent == enemyController.transform)
+            if (collider.transform.parent.name == enemyCtrl.name)
             {
-                this.enemies.Remove(enemyController);
+                this.enemies.Remove(enemyCtrl);
                 return;
             }
         }
     }
+
     protected virtual void FindNearest()
     {
         float nearestDistance = Mathf.Infinity;
         float enemyDistance;
-        foreach(EnemyController enemyController in this.enemies)
+        foreach (EnemyController enemyCtrl in this.enemies)
         {
-            enemyDistance = Vector3.Distance(transform.position, enemyController.transform.position);
-            if(enemyDistance < nearestDistance)
+            if (!this.CanSeeTarget(enemyCtrl)) continue;
+
+            enemyDistance = Vector3.Distance(transform.position, enemyCtrl.transform.position);
+            if (enemyDistance < nearestDistance)
             {
                 nearestDistance = enemyDistance;
-                this.nearest = enemyController;
+                this.nearest = enemyCtrl;
             }
         }
     }
+
+    protected virtual bool CanSeeTarget(EnemyController target)
+    {
+        Vector3 directionToTarget = target.transform.position - transform.position;
+        float distanceToTarget = directionToTarget.magnitude;
+
+        if (Physics.Raycast(transform.position, directionToTarget, out RaycastHit hitInfo, distanceToTarget, obstacleLayerMask, QueryTriggerInteraction.Ignore))
+        {
+            Vector3 directionToCollider = hitInfo.point - transform.position;
+            float distanceToCollider = directionToCollider.magnitude;
+
+            Debug.DrawRay(transform.position, directionToCollider.normalized * distanceToCollider, Color.red);
+            return false;
+        }
+
+        Debug.DrawRay(transform.position, directionToTarget.normalized * distanceToTarget, Color.green);
+        return true;
+    }
+
     protected virtual void RemoveDeadEnemy()
     {
-        foreach(EnemyController enemyCtrl in this.enemies){
+        foreach (EnemyController enemyCtrl in this.enemies)
+        {
             if (enemyCtrl.EnemyDamageReceiver.IsDead())
             {
-                if(enemyCtrl == this.nearest) this.nearest = null;  
+                if (enemyCtrl == this.nearest) this.nearest = null;
                 this.enemies.Remove(enemyCtrl);
                 return;
             }
