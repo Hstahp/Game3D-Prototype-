f10e318266dfe608b5ac7c56c2226fc716431338
Huy Phatt <phatle219@gmail.com>
2025-08-29 11:41:26 +0700

    E77

---
+++ Assets/_Data/UI/ItemDrop/ItemsDropManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class ItemsDropManager : SaiSingleton<ItemsDropManager>
+{
+    [SerializeField] protected ItemsDropSpawner spawner;
+    public ItemsDropSpawner Spawner => spawner;
+
+    protected override void LoadComponents()
+    {
+        base.LoadComponents();
+        this.LoadSpawner();
+    }
+
+    protected virtual void LoadSpawner()
+    {
+        if (this.spawner != null) return;
+        this.spawner = GetComponent<ItemsDropSpawner>();
+        Debug.Log(transform.name + ": LoadSpawner", gameObject);
+    }
+
+    public virtual void Drop(ItemCode itemCode, int dropCount)
+    {
+        //Create Item from Pool
+        //make that item value
+    }
+}
